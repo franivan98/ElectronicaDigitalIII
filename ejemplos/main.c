17dbd2e73c2cd6967c983e9b2c2f9d33c05bdb36
Franco Mamani <franko15mamani@gmail.com>
2024-08-19 18:21:31 -0300

    introduccionPunteros

---
+++ ejemplos/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+    int num;
+    num=78;
+    printf("\n La direccion de memoria de la variable num es %p", &num);
+    printf("\n El valor de la variable num es %d\n", num);
+}
